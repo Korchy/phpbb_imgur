07e5c260416ae0ae0a687738cb8b1449275a8790
Korchy <Korchy@users.noreply.github.com>
2015-09-24 10:58:38 +0300

    Create imgurredirect.php

---
+++ imgurredirect.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<link rel="icon" href="../../favicon.ico" type="image/x-icon"> 
+<link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
+<?php
+echo "<title>imgur redirection</title>";
+?>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<script type="text/javascript" src="imgurredirect.js"></script>
+</head>
+<body>
+<table border=0 width=100% height=100%>
+<tr><td align=center valign=center>
+Авторизация пройдена успешно.
+</td></td>
+</table>
+</body></html>
